262b3cdbe0520bff71f87505045f174c66f44797
Caner <5658514@qq.com>
2022-01-14 10:18:25 +0800

    Signed-off-by: Caner <5658514@qq.com>

--- test-AT.js
+++ test-AT.js
@@ -3,7 +3,7 @@ SerialPort.list().then(res => {
     console.log(res);
 })
 const Readline = require('@serialport/parser-readline')
-const port = new SerialPort('/dev/ttyS0')
+const port = new SerialPort('/dev/ttyUSB2')
 
 const parser = port.pipe(new Readline({ delimiter: '\r\n' }))
 parser.on('data', (data) => {
